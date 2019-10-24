394e29f29b1d45be39c1e010762447667da59791
Bremen <bvance@iastate.edu>
2019-10-24 08:09:19 -0500

    testing..

---
+++ js/finalproject.js
@@ -0,0 +1,15 @@
+
+
+function searchTerms() {
+  let dataFile = new XMLHttpRequest;
+  dataFile.open('GET', dataFile, true);
+  dataFile.onload = function (){
+    let text = dataFile
+    console.log(text)
+  };
+  // const allWords = async () => {
+  //
+  // }
+  // serachTerm = document.getElementById("searchBar").value;
+  //
+};
