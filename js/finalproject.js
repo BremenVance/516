6120b22f4d59bce10e50bf35c252070d1a516c3c
Bremen <bvance@iastate.edu>
2019-11-06 10:47:00 -0600

    Update to projectd
    addressed double vowel.

--- js/finalproject.js
+++ js/finalproject.js
@@ -36,3 +36,6 @@ function findTerm() {
     dataFile.send();
   }
 }
+
+var svgContainer = d3.select("#display").append("svg").attr("width", 200).attr("height", 200);
+var circle = svgContainer.append("circle").attr("cx", 30).attr("cy", 30).attr("r");
