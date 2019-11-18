9b9e98788f0b1a544c257c3c7b991a091aa96f22
Bremen <bvance@iastate.edu>
2019-11-18 12:12:19 -0600

    Finished project e and made some small updates to first 3 projects.

--- js/projecta.js
+++ js/projecta.js
@@ -1,25 +1,30 @@
-let assumedHigh = 100;
-let assumedLow = 1;
-let compGuess = ((assumedHigh + assumedLow)/2).toFixed(0);
-let userResponse = prompt("Is your number " + compGuess);
+function guessingGame(){
+  let assumedHigh = 100;
+  let assumedLow = 1;
+  let compGuess = ((assumedHigh + assumedLow)/2).toFixed(0);
+  let userResponse = prompt("Is your number " + compGuess);
 
-while (userResponse != "yes") {
-    if (userResponse == ">") {
-      assumedLow = compGuess;
-      diff = ((assumedHigh - assumedLow)/2).toFixed(0);
-      compGuess = parseInt(diff) + parseInt(compGuess);
-      userResponse = prompt("Is your number " + compGuess);
-    }
-
-      else if (userResponse == "<") {
-        assumedHigh = compGuess;
+  while (userResponse != "yes") {
+      if (userResponse == ">") {
+        assumedLow = compGuess;
         diff = ((assumedHigh - assumedLow)/2).toFixed(0);
-        compGuess = parseInt(compGuess) - parseInt(diff);
+        compGuess = parseInt(diff) + parseInt(compGuess);
         userResponse = prompt("Is your number " + compGuess);
       }
 
-}
+        else if (userResponse == "<") {
+          assumedHigh = compGuess;
+          diff = ((assumedHigh - assumedLow)/2).toFixed(0);
+          compGuess = parseInt(compGuess) - parseInt(diff);
+          userResponse = prompt("Is your number " + compGuess);
+        }
+      else {
+        userResponse = prompt("I ONLY Understand <,>, or yes, the guess is still " + compGuess);
+      }
 
-if (userResponse == "yes") {
-  alert("I got it! Your number is: " + compGuess);
+  }
+
+  if (userResponse == "yes") {
+    alert("I got it! Your number is: " + compGuess);
+  }
 }
