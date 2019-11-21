885f6ef5532c769eeaab801f5ab47e9efd6ebb83
Bremen <bvance@iastate.edu>
2019-11-20 20:25:58 -0600

    Working on final project.

--- js/finalproject.js
+++ js/finalproject.js
@@ -13,7 +13,15 @@ function searchTerms() {
 //add search term to a list and add to the page
 let termList = [];
 function addTerm() {
-  termList.push(text);
+  let textClean = text;
+  textClean = textClean.toLowerCase();
+  textClean = textClean.replace(/\W/g," ")//removes non word characters
+  textClean = textClean.replace(/\d/g," ")//removes numbers
+  textClean = textClean.split(" "); //splits string into items in an array based on a space
+  textClean = textClean.filter(function (word) { //removes empty items in the list
+    return word !="";
+  });
+  termList.push(textClean);
   init();
   document.getElementById("termList").innerHTML = termList;
 };
@@ -25,17 +33,51 @@ function clearTerms() {
 };
 
 //find term in data file
-function findTerm() {
-  let dataFile = new XMLHttpRequest();
-  dataFile.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("data").innerHTML =
-            this.responseText;
-      }
-    dataFile.open("GET", "data/test.txt", true);
-    dataFile.send();
-  }
-}
+// function findTerm() {
+//   let dataFile = new XMLHttpRequest();
+//   dataFile.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//       document.getElementById("data").innerHTML =
+//             this.responseText;
+//       }
+//     dataFile.open("GET", "data/test.txt", true);
+//     dataFile.send();
+//   }
+// }
 
-var svgContainer = d3.select("#display").append("svg").attr("width", 200).attr("height", 200);
-var circle = svgContainer.append("circle").attr("cx", 30).attr("cy", 30).attr("r");
+// d3 data loading
+    d3.json("data/multimodalcollocations.json", function(data){
+
+        console.log(data);
+
+        collocations = JSON.parse(data);
+        console.log(collocations);
+        // let visual = data.art;
+        // console.log(art);
+        wordArray = [];
+        for (w in data){
+          wordArray.push(w);
+        }
+        console.log(wordArray);
+
+
+        var svg = d3.select("#display").append("svg").attr("height","100%").attr("width","100%");
+
+        // svg.selectAll("rect")
+        //       .data(dataArray)
+        //       .enter().append("rect")
+        //                 .attr("height",function(d,i){ return d*15; })
+        //                 .attr("width","50")
+        //                 .attr("fill","pink")
+        //                 .attr("x",function(d,i){ return 60*i; })
+        //                 .attr("y",function(d,i){ return 300-(d*15); });
+        // console.log(Object.keys(data));
+        // console.log(data.visual);
+        // i = 0;
+        // while (i < 5){
+        //   console.log(data."visual");
+        //   i++;
+        // }
+
+        svg.selectAll("rect")
+    });
